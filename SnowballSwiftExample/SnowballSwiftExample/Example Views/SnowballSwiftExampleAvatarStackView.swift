snowball-tools/SnowballSwiftKit

fix: remove uikit deps

SnowballSwiftExample/SnowballSwiftExample/Example Views/SnowballSwiftExampleAvatarStackView.swift
@@ -5,7 +5,6 @@
 //  Created by Vivian Phung on 5/22/23.
 //
 
-import UIKit
 import SwiftUI
 import SnowballSwiftKit
 import SnowballAssetKit
@@ -15,29 +14,29 @@ struct SnowballSwiftExampleAvatarStackView: View {
         VStack {
             SnowballAvatarRemoteStack([SnowballAvatarRemoteModel(imageURL: "https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg"), SnowballAvatarRemoteModel(imageURL: "https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg")])
 
-            SnowballAvatarStack([SnowballImage(.metamask),
-                                 SnowballImage(.coinbaseWallet),
-                                 SnowballImage(.walletConnect),
-                                 SnowballImage(.argent),
-                                 SnowballImage(.brave)])
+            SnowballAvatarStack([Image(.metamask),
+                                 Image(.coinbaseWallet),
+                                 Image(.walletConnect),
+                                 Image(.argent),
+                                 Image(.brave)])
                 .frame(height: 40)
 
-            SnowballAvatarStack([SnowballImage(.metamask),
-                                 SnowballImage(.coinbaseWallet),
-                                 SnowballImage(.walletConnect),
-                                 SnowballImage(.argent)])
+            SnowballAvatarStack([Image(.metamask),
+                                 Image(.coinbaseWallet),
+                                 Image(.walletConnect),
+                                 Image(.argent)])
                 .frame(height: 32)
 
-            SnowballAvatarStack([SnowballImage(.metamask),
-                                 SnowballImage(.coinbaseWallet),
-                                 SnowballImage(.walletConnect),
-                                 SnowballImage(.argent)])
+            SnowballAvatarStack([Image(.metamask),
+                                 Image(.coinbaseWallet),
+                                 Image(.walletConnect),
+                                 Image(.argent)])
                 .frame(height: 24)
 
-            SnowballAvatarStack([SnowballImage(.metamask),
-                                 SnowballImage(.coinbaseWallet),
-                                 SnowballImage(.walletConnect),
-                                 SnowballImage(.argent)])
+            SnowballAvatarStack([Image(.metamask),
+                                 Image(.coinbaseWallet),
+                                 Image(.walletConnect),
+                                 Image(.argent)])
                 .frame(height: 20)
 
         }

snowball-tools/SwiftKit

SnowballSwiftExample/SnowballSwiftExample/Example Views/SnowballSwiftExampleAvatarStackView.swift
@@ -5,7 +5,6 @@
 //  Created by Vivian Phung on 5/22/23.
 //
 
-import UIKit
 import SwiftUI
 import SnowballSwiftKit
 import SnowballAssetKit
@@ -15,29 +14,29 @@ struct SnowballSwiftExampleAvatarStackView: View {
         VStack {
             AsyncAvatarStack([SnowballAvatarRemoteModel(imageURL: "https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg"), SnowballAvatarRemoteModel(imageURL: "https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg")])
 
-            AvatarStack([SnowballImage(.metamask),
-                                 SnowballImage(.coinbaseWallet),
-                                 SnowballImage(.walletConnect),
-                                 SnowballImage(.argent),
-                                 SnowballImage(.brave)])
+            AvatarStack([Image(.metamask),
+                                 Image(.coinbaseWallet),
+                                 Image(.walletConnect),
+                                 Image(.argent),
+                                 Image(.brave)])
                 .frame(height: 40)
 
-            AvatarStack([SnowballImage(.metamask),
-                                 SnowballImage(.coinbaseWallet),
-                                 SnowballImage(.walletConnect),
-                                 SnowballImage(.argent)])
+            AvatarStack([Image(.metamask),
+                                 Image(.coinbaseWallet),
+                                 Image(.walletConnect),
+                                 Image(.argent)])
                 .frame(height: 32)
 
-            AvatarStack([SnowballImage(.metamask),
-                                 SnowballImage(.coinbaseWallet),
-                                 SnowballImage(.walletConnect),
-                                 SnowballImage(.argent)])
+            AvatarStack([Image(.metamask),
+                                 Image(.coinbaseWallet),
+                                 Image(.walletConnect),
+                                 Image(.argent)])
                 .frame(height: 24)
 
-            AvatarStack([SnowballImage(.metamask),
-                                 SnowballImage(.coinbaseWallet),
-                                 SnowballImage(.walletConnect),
-                                 SnowballImage(.argent)])
+            AvatarStack([Image(.metamask),
+                                 Image(.coinbaseWallet),
+                                 Image(.walletConnect),
+                                 Image(.argent)])
                 .frame(height: 20)
 
         }