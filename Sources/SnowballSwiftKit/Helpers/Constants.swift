311c5135c7b52058d2e698b3c4dfadd49e14f004
Vivian Phung <dev+github@vivianphung.com>
2024-06-26 02:18:14 -0400

    avatar

---
+++ Sources/SnowballSwiftKit/Helpers/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  SwiftKit
+//
+//  Created by Vivian Phung on 6/26/24.
+//
+
+let avatarImageUrlString: String = "https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s176-c-k-c0x00ffffff-no-rj"
--- Sources/SnowballSwiftKit/Views/AvatarStack.swift
+++ Sources/SnowballSwiftKit/Views/AvatarStack.swift
@@ -1,6 +1,6 @@
 //
 //  SnowballAvatarStack.swift
-//  
+//  SwiftKit
 //
 //  Created by Vivian Phung on 5/29/23.
 //
@@ -8,33 +8,68 @@
 import SwiftUI
 import SnowballAssetKit
 
-public struct AvatarStack: View {
-    var avatars: [Image]
-    var number: Int
-
-    public init(_ avatars: [Image]) {
-        self.number = avatars.count - 4
-        self.avatars = Array(avatars.prefix(4))
+struct AvatarStack: View {
+    let avatars: [URL?]
+    let size: CGFloat
+    let spacing: CGFloat
+    let maxDisplayed: Int
+    
+    init(avatars: [URL?], size: CGFloat = 40, spacing: CGFloat = -15, maxDisplayed: Int = 4) {
+        self.avatars = avatars
+        self.size = size
+        self.spacing = spacing
+        self.maxDisplayed = maxDisplayed
     }
-
-    public var body: some View {
-        HStack(spacing: -10) {
-            ForEach(Array(zip(avatars.indices, avatars)), id: \.0) { _, avatar in
-               AvatarView(avatar)
+    
+    var body: some View {
+        HStack(spacing: spacing) {
+            ForEach(0..<min(avatars.count, maxDisplayed), id: \.self) { index in
+                Avatar(imageURL: avatars[index], size: size, showPlaceholder: avatars[index] == nil)
             }
-            if self.number > 0 {
-               AvatarView(self.number)
+            if avatars.count > maxDisplayed {
+                Avatar(size: size)
             }
         }
     }
 }
 
-struct SnowballAvatarStack_Previews: PreviewProvider {
+struct AvatarStack_Previews: PreviewProvider {
+    static let sampleAvatars: [URL?] = [
+        URL(string: avatarImageUrlString),
+        URL(string: avatarImageUrlString),
+        nil,
+        URL(string: avatarImageUrlString),
+        URL(string: avatarImageUrlString),
+        URL(string: avatarImageUrlString)
+    ]
+    
     static var previews: some View {
-        AvatarStack([
-            Image(.sol),
-            Image(.metamask)
-        ])
-        .frame(height: 32)
+        Group {
+            // Default configuration
+            AvatarStack(avatars: sampleAvatars)
+                .previewLayout(.sizeThatFits)
+                .padding()
+                .previewDisplayName("Default Group")
+            
+            // Larger size and different spacing
+            AvatarStack(avatars: sampleAvatars, size: 60, spacing: -20)
+                .previewLayout(.sizeThatFits)
+                .padding()
+                .previewDisplayName("Large Avatars")
+            
+            // Fewer avatars
+            AvatarStack(avatars: Array(sampleAvatars.prefix(3)))
+                .previewLayout(.sizeThatFits)
+                .padding()
+                .previewDisplayName("Few Avatars")
+            
+            // Dark mode
+            AvatarStack(avatars: sampleAvatars)
+                .previewLayout(.sizeThatFits)
+                .padding()
+                .background(Color.black)
+                .environment(\.colorScheme, .dark)
+                .previewDisplayName("Dark Mode Group")
+        }
     }
 }
